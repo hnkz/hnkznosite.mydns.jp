f922c07d146beddc9f30a5e0d015b34abd32ad0d
Kazuki Hanai <cs16075@s.inf.shizuoka.ac.jp>
2016-11-27 12:15:04 +0900

    add function
    add

--- script.js
+++ script.js
@@ -1,42 +1,116 @@
-var name = "hanakazu: ~ $ ";
-var msg = "<h1>Welcome to hnkznosite.mydns.jp<h1>"+ name +"";
-var roopFlag = false;
+//時間の取得の初期化
+dd = new Date();
+hours = dd.getHours();
+minutes = dd.getMinutes();
+seconds = dd.getSeconds();
+var time = hours + ":" + minutes + ":" + seconds;
+
+//ディレクトリの初期化
+var dir = "";
+
+//ユーザ名の初期化
+var name = "hanakazu ";
+
+//コマンドの初期化
+var command = "";
+
+//home時のlsコマンドを初期化
+var ls_home = ["<span class=\"nav\">home</span>", "<span class=\"nav\">work</span>", "<span class=\"nav\">git</span>","<span class=\"nav\">link</span>"];
+
+//navのリストを初期化
+var navList = ["home", "work", "git", "link"];
+
+
+//表示するメッセージの初期化
+var msg = "<h1>Welcome to hnkznosite.mydns.jp<h1>"+ name + time + " <span class=\"dir\">/" + dir +  "</span> $ ";
+
+//tag感知フラグ
 var tagFlag = false;
+
+//文字数カウンタ
 var count = 0;
 
+//最初の表示
 function disp(){
-
 	var type = msg.substring(0, count);
   var lastType = type.slice(-1);
 
-  if(lastType === "<"){ roopFlag = true; }
-  if(lastType === ">"){ roopFlag = false; }
+	if(lastType === "<"){ tagFlag = true; }
+  if(lastType === ">"){ tagFlag = false; }
 
-  if(!roopFlag){
+	if(!tagFlag){
   	document.getElementById("all").innerHTML = type;
   	count ++;
-  }
-  else{
+  } else {
     count ++;
   }
 
 	var rep = setTimeout("disp()", 15);
+
 	if(count > msg.length){
-		if(roopFlag){ count = 0; }
-		else{ clearTimeout(rep); }
+		clearTimeout(rep);
 	}
 }
 
-var command = "";
-
+//ユーザがキーを入力したとき
 document.onkeydown = function(e) {
+
+	//時間の取得
+	dd = new Date();
+	hours = dd.getHours();
+	minutes = dd.getMinutes();
+	seconds = dd.getSeconds();
+	var time = hours + ":" + minutes + ":" + seconds;
+
 	if (event.keyCode == 13) {
-		msg = msg + command + "<br>" + name;
-		command = "";
-		document.getElementById("all").innerHTML = msg;
-	}
-	else if (event.keyCode == 8) {
+		if (command == "") {
+			document.getElementById("all").innerHTML = msg;
+		} else {
+			if (command.match(/cd/)) {
+				if (dir == "") {
+					for(var i = 0; i < navList.length; i++){
+						reg = new RegExp(navList[i]);
+						if(command.match(reg)){
+							dir = navList[i];
+						}
+					}
+					msg = msg + command + "<br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+				} else {
+					if (command.match("..")) {
+						dir = "";
+						msg = msg + command + "<br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+					} else {
+						msg = msg + "<br>-hnkz: cd: No such file or directory<br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+					}
+				}
+			} else if ( command == "ls") {
+				if(dir == ""){
+					var lsList = ls_home[0] + " " + ls_home[1] + " " + ls_home[2] + " " + ls_home[3];
+					msg = msg + command + "<br>" + lsList + "<br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+				} else {
+					msg = msg + command + "<br><br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+				}
+			} else if ( command == "clear") {
+				msg = "<h1>Welcome to hnkznosite.mydns.jp<h1>"+ name + time + " <span class=\"dir\">/" + dir +  "</span> $ "
+			} else {
+				msg = msg + command + "<br>" + "-hnkz: " + command + ": command not found" + "<br>" + name + time + " <span class=\"dir\">/" + dir + "</span> $ ";
+			}
+
+			command = "";
+			document.getElementById("all").innerHTML = msg;
+		}
+	} else if (event.keyCode == 8) {
 		command = command.substr( 0, command.length-1 );
+		document.getElementById("all").innerHTML = msg + command;
+	} else if (event.keyCode == 189) {
+		command = command + "_";
+		document.getElementById("all").innerHTML = msg + command;
+	} else if (event.keyCode == 190) {
+		command = command + ".";
+		document.getElementById("all").innerHTML = msg + command;
+	}
+	else if (event.keyCode == 191) {
+		command = command + "/";
 		document.getElementById("all").innerHTML = msg + command;
 	}
   else{
